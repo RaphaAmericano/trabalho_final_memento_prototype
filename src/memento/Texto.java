73d2fa29b033e9124f9db1a644c26c1f3a47333a
RaphaAmericano <raphael@raphaelamericano.com.br>
2017-07-02 22:40:32 -0300

    Primeiro commit

---
+++ src/memento/Texto.java
@@ -0,0 +1,21 @@
+package memento;
+
+public class Texto {
+	protected String texto;
+	TextoCareTaker caretaker;
+	//Construtor
+	public Texto(){
+		caretaker = new TextoCareTaker();
+		texto = new String();
+	}
+	public void escreverTexto( String novoTexto){
+		caretaker.adicionarMemento(new TextoMemento(texto));
+		texto += novoTexto;
+	}
+	public void desfazerEscrita(){
+		texto = caretaker.getUltimoEstadoSalvo().getTextoSalvo();
+	}
+	public void mostrarTexto() {
+		System.out.println(texto);
+	}
+}
---
+++ src/memento/TextoMemento.java
@@ -0,0 +1,12 @@
+package memento;
+
+public class TextoMemento {
+	protected String estadoTexto;
+	
+	public TextoMemento(String texto){
+		estadoTexto = texto;
+	}
+	public String getTextoSalvo(){
+		return estadoTexto;
+	}
+}
